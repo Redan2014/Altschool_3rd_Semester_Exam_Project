7449c5923dfc219eace0f5645c228054c1361487
Owhologbo P. O. Daniel <princedo22002@gmail.com>
2023-03-18 15:55:07 +0100

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,18 +1,3 @@
-provider "kubernetes" {
-  # load_config_file       = "false"
-  host                   = data.aws_eks_cluster.mongoapp-cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.mongoapp-cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.mongoapp-cluster.certificate_authority.0.data)
-}
-  
-data "aws_eks_cluster" "mongoapp-cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "mongoapp-cluster" {
-  name = module.eks.cluster_id
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.10.0"
@@ -32,8 +17,8 @@ module "eks" {
     {
         instance_type = "t3.medium"
         min_size = 1
-        max_size = 3
-        asg_desired_capacity = 2
+        max_size = 4
+        asg_desired_capacity = 3
     }
   ]
 }
